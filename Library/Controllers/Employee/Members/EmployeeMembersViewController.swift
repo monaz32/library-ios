538fbc216080c497d88571bd41f85205266f56c3
Paco Lee <paco.lee@twotalltotems.com>
2018-03-27 12:39:55 -0700

    Add EmployeeMembers view controller

---
+++ Library/Controllers/Employee/Members/EmployeeMembersViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeMembersViewController.swift
+//  Library
+//
+//  Created by Paco Lee on 2018-03-27.
+//  Copyright © 2018 Paco Lee. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeMembersViewController: UIViewController {
+    static let identifier = "EmployeeMembersViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
