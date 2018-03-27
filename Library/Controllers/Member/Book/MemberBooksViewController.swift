2303d19e46cb56e1b2ad30e8bb31b2ae3692f107
Paco Lee <paco.lee@twotalltotems.com>
2018-03-27 14:22:07 -0700

    Modify Members My account and make MembersBook view controller

---
+++ Library/Controllers/Member/Book/MemberBooksViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemberBooksViewController.swift
+//  Library
+//
+//  Created by Paco Lee on 2018-03-27.
+//  Copyright © 2018 Paco Lee. All rights reserved.
+//
+
+import UIKit
+
+class MemberBooksViewController: UIViewController {
+    static let identifier = "MemberBooksViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
