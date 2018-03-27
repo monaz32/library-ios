0e3dfd5ac444ad9c3d8c6e2990a8b102611b70b2
Paco Lee <paco.lee@twotalltotems.com>
2018-03-27 16:30:33 -0700

    Create book room view controllers

---
+++ Library/Controllers/Member/Room/MemberRoomViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MemberRoomViewController.swift
+//  Library
+//
+//  Created by Paco Lee on 2018-03-27.
+//  Copyright © 2018 Paco Lee. All rights reserved.
+//
+
+import UIKit
+
+class MemberRoomViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
