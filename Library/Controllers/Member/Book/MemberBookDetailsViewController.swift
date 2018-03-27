00565fd6f0faf86eb4fde7a102dd7e55ea4fc0e6
Paco Lee <paco.lee@twotalltotems.com>
2018-03-27 16:12:01 -0700

    Create book details and book review view controllers

---
+++ Library/Controllers/Member/Book/MemberBookDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MemberBookDetailsViewController.swift
+//  Library
+//
+//  Created by Paco Lee on 2018-03-27.
+//  Copyright © 2018 Paco Lee. All rights reserved.
+//
+
+import UIKit
+
+class MemberBookDetailsViewController: UIViewController {
+    static let identifier = "MemberBookDetailsViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ Library/Controllers/Member/Book/MemberBookReviewViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MemberBookReviewViewController.swift
+//  Library
+//
+//  Created by Paco Lee on 2018-03-27.
+//  Copyright © 2018 Paco Lee. All rights reserved.
+//
+
+import UIKit
+
+class MemberBookReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
