8de9d2adae587d1026881724905d3109cf111e3e
Paco Lee <paco.lee@twotalltotems.com>
2018-03-27 12:26:59 -0700

    Added EmployeeBooks view controllers

---
+++ Library/Controllers/Employee/Book/EmployeeAddBookViewController.swift
@@ -0,0 +1,18 @@
+//
+//  EmployeeAddBookViewController.swift
+//  Library
+//
+//  Created by Paco Lee on 2018-03-26.
+//  Copyright © 2018 Paco Lee. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeAddBookViewController: UIViewController {
+    static let identifier = "EmployeeAddBookViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
---
+++ Library/Controllers/Employee/Book/EmployeeBookFilterViewController.swift
@@ -0,0 +1,17 @@
+//
+//  EmployeeBookFilterViewController.swift
+//  Library
+//
+//  Created by Paco Lee on 2018-03-26.
+//  Copyright © 2018 Paco Lee. All rights reserved.
+//
+
+import UIKit
+
+class EmployeeBookFilterViewController: UIViewController {
+    static let identifier = "EmployeeBookFilterViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
