339da8959ae85d3ee1db1f34c682b08f329ff1ea
Paco Lee <paco.lee@twotalltotems.com>
2018-03-28 15:49:29 -0700

    get account info

--- Library/Controllers/General/Account/MemberAccountViewController.swift
+++ Library/Controllers/General/Account/MemberAccountViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MemberViewController.swift
+//  MemberAccountViewController.swift
 //  Library
 //
 //  Created by Paco Lee on 2018-03-25.
@@ -8,10 +8,47 @@
 
 import UIKit
 
-class MemberViewController: UIViewController {
+class MemberAccountViewController: UIViewController {
     static let identifier = "MemberAccountViewController"
-
+    
+    @IBOutlet var bookTableView: UITableView!
+    @IBOutlet var pastHistoryTableView: UITableView!
+    @IBOutlet var nameTextField: UITextField!
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var phoneNumberTextField: UITextField!
+    @IBOutlet var finesLabel: UILabel!
+    
+    var member: Member!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        setup()
+    }
+    
+    func setup() {
+        let id = UserDefaults.standard.integer(forKey: "id")
+        MemberService.sharedService.getMember(id: id) { (result) in
+            if result.isSuccess, let member = result.value {
+                self.member = member
+                self.nameTextField.text = member.name
+                self.emailTextField.text = member.email
+                self.phoneNumberTextField.text = member.phoneNumber
+                
+                if let fines = member.fines {
+                    self.finesLabel.text = "Total Fines: \(fines)"
+                } else {
+                    self.finesLabel.text = "Total Fines: 0"
+                }
+            }
+        }
+    }
+    
+    @IBAction func updateAction(_ sender: Any) {
+        
+    }
+    
+    
 }
