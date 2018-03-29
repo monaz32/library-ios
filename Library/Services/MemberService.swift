cdade72b0ffa71c72bf6f5c8e35a1b7ea8012fa6
Mona Zhao <n4u9a@ugrad.cs.ubc.ca>
2018-03-29 11:03:11 -0700

    addMember now returns member id

--- Library/Services/MemberService.swift
+++ Library/Services/MemberService.swift
@@ -42,11 +42,13 @@ class MemberService {
         }
     }
     
-    func addMember(phoneNum: String, email: String, name: String, password: String, completion: @escaping (Result<Bool>) -> Void) {
+    func addMember(phoneNum: String, email: String, name: String, password: String, completion: @escaping (Result<Int>) -> Void) {
         APIClient.sharedClient.request(Router.addMember(phoneNum: phoneNum, email: email, name: name, password: password)) { (response) in
             switch response {
-            case .success:
-                completion(Result.success(true))
+            case .success(let result):
+                if let json = result as? [[String: Any]], let id = json[0]["newID"] as? Int {
+                    completion(Result.success(id))
+                }
             case .failure(let error):
                 completion(Result.failure(error))
             }
