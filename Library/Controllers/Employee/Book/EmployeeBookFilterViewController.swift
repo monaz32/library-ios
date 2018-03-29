4e4f3ffc8c6be1e22d72afa9ccd007bb9edac371
Paco Lee <paco.lee@twotalltotems.com>
2018-03-29 12:56:56 -0700

    Get books for employee

--- Library/Controllers/Employee/Book/EmployeeBookFilterViewController.swift
+++ Library/Controllers/Employee/Book/EmployeeBookFilterViewController.swift
@@ -11,7 +11,54 @@ import UIKit
 class EmployeeBookFilterViewController: UIViewController {
     static let identifier = "EmployeeBookFilterViewController"
 
+    @IBOutlet var titleTextField: UITextField!
+    @IBOutlet var authorTextField: UITextField!
+    @IBOutlet var publisherTextField: UITextField!
+    @IBOutlet var genreTextField: UITextField!
+    
+    var delegate: EmployeeBooksViewControllerDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func setup(delegate: EmployeeBooksViewControllerDelegate) {
+        self.delegate = delegate
+    }
+    
+    @IBAction func addFiltersAction(_ sender: Any) {
+        var title: String
+        var author: String
+        var publisher: String
+        var genre: String
+        
+        if titleTextField.text == nil {
+            title = ""
+        } else {
+            title = titleTextField.text!
+        }
+        
+        if authorTextField.text == nil {
+            author = ""
+        } else {
+            author = authorTextField.text!
+        }
+        
+        if publisherTextField.text == nil {
+            publisher = ""
+        } else {
+            publisher = publisherTextField.text!
+        }
+        
+        if genreTextField.text == nil {
+            genre = ""
+        } else {
+            genre = genreTextField.text!
+        }
+        
+        let bookFilter = BookFilter(title: title, author: author, publisher: publisher, genre: genre)
+        
+        delegate.changeFilter(bookFilter: bookFilter)
+        navigationController?.popViewController(animated: true)
     }
 }
--- Library/Controllers/Employee/Book/EmployeeBooksViewController.swift
+++ Library/Controllers/Employee/Book/EmployeeBooksViewController.swift
@@ -8,13 +8,45 @@
 
 import UIKit
 
+protocol EmployeeBooksViewControllerDelegate {
+    func changeFilter(bookFilter: BookFilter)
+}
+
 class EmployeeBooksViewController: UIViewController {
 
     @IBOutlet var bookTableView: UITableView!
     
+    var bookFilter = BookFilter(title: "", author: "", publisher: "", genre: "")
+    var books = [Book]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setupTableView()
+    }
+    
+    private func setupTableView() {
+        bookTableView.delegate = self
+        bookTableView.dataSource = self
+        bookTableView.register(UINib(nibName: MemberBooksTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: MemberBooksTableViewCell.identifier)
+        bookTableView.rowHeight = UITableViewAutomaticDimension
+        bookTableView.estimatedRowHeight = 44
+        bookTableView.tableFooterView = UIView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        BookService.sharedService.getBooks(title: bookFilter.title, author: bookFilter.author, publisher: bookFilter.publisher, genre: bookFilter.genre) { (result) in
+            if result.isSuccess, let books = result.value{
+                self.books = books
+                self.bookTableView.reloadData()
+            } else {
+                let alertController = UIAlertController(title: "Error 422", message: "Unprocessable Entity", preferredStyle: .alert)
+                let action = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+                }
+                alertController.addAction(action)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
     }
     
     @IBAction func addBookAction(_ sender: Any) {
@@ -24,6 +56,41 @@ class EmployeeBooksViewController: UIViewController {
     
     @IBAction func addFiltersAction(_ sender: Any) {
         let vc = UIStoryboard.init(name: "EmployeeBooks", bundle: nil).instantiateViewController(withIdentifier: EmployeeBookFilterViewController.identifier) as! EmployeeBookFilterViewController
+        vc.setup(delegate: self)
         navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+// MARK: - Table view delegate
+
+extension EmployeeBooksViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let vc = UIStoryboard(name: "MemberBooks", bundle: nil).instantiateViewController(withIdentifier: MemberBookDetailsViewController.identifier) as! MemberBookDetailsViewController
+//        let book = books[indexPath.row]
+//        vc.config(book: book)
+//        navigationController?.pushViewController(vc, animated: true)
+    }
+}
+
+// MARK: - Table view data source
+
+extension EmployeeBooksViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MemberBooksTableViewCell.identifier, for: indexPath) as! MemberBooksTableViewCell
+        let book = books[indexPath.row]
+        cell.config(book: book)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return books.count
+    }
+}
+
+// MARK: - EmployeeBooksViewControllerDelegate
+
+extension EmployeeBooksViewController: EmployeeBooksViewControllerDelegate {
+    func changeFilter(bookFilter: BookFilter) {
+        self.bookFilter = bookFilter
     }
 }
