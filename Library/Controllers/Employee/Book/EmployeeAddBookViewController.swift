9e8f15aca8a01976a165e4f24819f2fd7df9c960
Mona Zhao <n4u9a@ugrad.cs.ubc.ca>
2018-03-29 17:03:44 -0700

    Added some guard on adding book page

--- Library/Controllers/Employee/Book/EmployeeAddBookViewController.swift
+++ Library/Controllers/Employee/Book/EmployeeAddBookViewController.swift
@@ -24,33 +24,57 @@ class EmployeeAddBookViewController: UIViewController {
     }
     
     @IBAction func addBookAction(_ sender: Any) {
-        guard let isbn = isbnTextField.text, !isbn.isEmpty, isbn.trimmingCharacters(in: .whitespaces).count > 0 else {
-            print("ISBN text field is empty")
+        guard let isbn = isbnTextField.text, !isbn.isEmpty, isbn.trimmingCharacters(in: .whitespaces).count > 0, let _ = Int(isbn) else {
+            let alertController = UIAlertController(title: "Error", message: "Invalid ISBN", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            }
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
             return
         }
         
-        guard let branchNum = branchNumTextField.text, !branchNum.isEmpty, branchNum.trimmingCharacters(in: .whitespaces).count > 0, let branchNumInt = Int(branchNum), branchNumInt >= 0 && branchNumInt <= 5 else {
-            print("Branch Number text field is empty")
+        guard let branchNum = branchNumTextField.text, !branchNum.isEmpty, branchNum.trimmingCharacters(in: .whitespaces).count > 0, let branchNumInt = Int(branchNum) else {
+            let alertController = UIAlertController(title: "Error", message: "Invalid Branch Number", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            }
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
             return
         }
         
         guard let title = titleTextField.text, !title.isEmpty, title.trimmingCharacters(in: .whitespaces).count > 0 else {
-            print("Status text field is empty")
+            let alertController = UIAlertController(title: "Error", message: "Title text field is empty", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            }
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
             return
         }
         
         guard let author = authorTextField.text, !author.isEmpty, author.trimmingCharacters(in: .whitespaces).count > 0 else {
-            print("Author text field is empty")
+            let alertController = UIAlertController(title: "Error", message: "Author text field is empty", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            }
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
             return
         }
         
         guard let publisher = publisherTextField.text, !publisher.isEmpty, publisher.trimmingCharacters(in: .whitespaces).count > 0 else {
-            print("Publisher text field is empty")
+            let alertController = UIAlertController(title: "Error", message: "Publisher text field is empty", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            }
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
             return
         }
         
         guard let genre = genreTextField.text, !genre.isEmpty, genre.trimmingCharacters(in: .whitespaces).count > 0 else {
-            print("Genre text field is empty")
+            let alertController = UIAlertController(title: "Error", message: "Genre text field is empty", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction) in
+            }
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
             return
         }
         
